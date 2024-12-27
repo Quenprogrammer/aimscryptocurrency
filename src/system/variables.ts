0b98498ed407205be4b9b5268e006fed69114cb3
QuenProgrammer <adamsadam3667@gmail.com>
2024-12-27 02:06:37 -0800

    Friday 27 december 2023 10:30AM work update

--- src/system/variables.ts
+++ src/system/variables.ts
@@ -4,3 +4,4 @@ export const exchangersPath:string='/assets/exchangers/';
 export const cryptoWalletTokens:string='/assets/cryptoWalletTokens/';
 export const currencies:string='/assets/Currency/';
 export const communities:string='/assets/socialIcons/';
+export const tools:string='/assets/socialIcons/';
